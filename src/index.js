83d0b33092aaea71aab44383d0d1459a812f20f2
notArobotAlex <not.a.robot.alex@gmail.com>
2025-06-29 19:09:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+const App = () => {
+  return (
+    <div>
+      <h1>ToDo App</h1>
+      <p>Привет, мир!</p>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
